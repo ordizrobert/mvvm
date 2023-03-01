ab95a7105ec9553f34842b4c3ce69202bcf92235
Robert G Ordiz <ordizrobert2011@gmail.com>
2023-03-01 09:35:06 +0800

    initial commit
    Mar 1

---
+++ mvvm/mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvm
+//
+//  Created by robert ordiz on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
